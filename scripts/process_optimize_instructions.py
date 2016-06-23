be896bd47115d51102469f8bf2ecaa1119f88b57
Alon Zakai <alonzakai@gmail.com>
2016-06-23 14:14:14 -0700

    more

--- scripts/process_optimize_instructions.py
+++ scripts/process_optimize_instructions.py
@@ -1,1 +1,16 @@
-waka
+#!/usr/bin/python
+
+import os, sys
+
+root = os.path.dirname(os.path.dirname(__file__))
+
+infile  = os.path.join(root, 'src', 'passes', 'OptimizeInstructions.wast')
+outfile = os.path.join(root, 'src', 'passes', 'OptimizeInstructions.wast.processed')
+
+out = open(outfile, 'w')
+
+for line in open(infile):
+  out.write('"' + line.strip() + '"\n')
+
+out.close()
+
